db76387b545ecd31068d733267f84ec1277aa3d3
kenflor2305 <c20245309.flores@csav.edu.ph>
2026-09-11 10:45:50 +0800

    Functions

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ console.log(`Name: ${myName}`);
 console.log(`Age: ${Age}`);
 console.log(`Number: ${myNumber}`);
 console.log(`Adress: ${myAdress}`);
+
+//Functions
+function greet(name){
+    return`Goodmorning, ${name}`;
+}
+console.log(greet(myName));
+
+function mdas(num1, num2){
+    let multiplacation = num1 * num2;
+    let division = num1 / num2;
+    let addition = num1 + num2;
+    let subtraction = num1 - num2;
+
+    return (`Values: ${num1} and ${num2}. Product: ${multiplacation}, Quotient: ${division}, Sum; ${addition},Diff: ${subtraction}`);
+}
+console.log (mdas(5,3))
