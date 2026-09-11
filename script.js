b8d264bba5ab67c6ded270af6b7d89f03e4776db
kenflor2305 <c20245309.flores@csav.edu.ph>
2026-09-11 09:21:35 +0800

    variables (let & const)

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello World!");
+
+const myName = 'Alken Flores';
+let Age = '20';
+const myNumber = 9638233541;
+const myAdress = 'Cadiz City';
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${Age}`);
+console.log(`Number: ${myNumber}`);
+console.log(`Adress: ${myAdress}`);
